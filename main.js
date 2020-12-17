3b1968be4d5b22375c1cd4dfb5c0a298dae08355
AryaPawar <72245486+AryaPawar@users.noreply.github.com>
2020-12-17 10:59:58 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -21,3 +21,42 @@ Webcam.set({
    function modelLoaded(){
    console.log('model Loaded');
    }
+   function check(){
+    img = document.getElementById('captured_image');
+    classifier.classify(img, gotResult);
+}
+function gotResult(error, results){
+   if(error){
+       console.error(error);
+   } else{
+       console.log(results);
+       document.getElementById("result_emotion_name").innerHTML = results[0].label;
+       document.getElementById("result_emotion_name2").innerHTML = results[1].label;
+       prediction1  = results[0].label;
+       prediction2 =  results[1].label;
+       if (results[0].label == "amazing")
+       {
+           document.getElementById("update_emoji").innerHTML = "&#128076;";
+       }
+       if (results[0].label == "amazing")
+       {
+           document.getElementById("update_emoji").innerHTML = "&#128077;";
+       } 
+       if (results[0].label == "best")
+       {
+           document.getElementById("update_emoji").innerHTML = "&#9996;";
+       } 
+       if (results[1].label == "victory")
+       {
+           document.getElementById("update_emoji2").innerHTML = "&#128076;";
+       }
+       if (results[1].label == "amazing")
+       {
+           document.getElementById("update_emoji2").innerHTML = "&#128077;";
+       }
+       if (results[1].label == "best")
+       {
+           document.getElementById("update_emoji2").innerHTML = "&#128548";
+       } 
+   }
+}
